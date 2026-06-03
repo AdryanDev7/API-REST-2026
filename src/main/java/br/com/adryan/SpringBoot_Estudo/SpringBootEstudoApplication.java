4f931aa73b1df2e0e4c4210d164915f28fde97e7
AdryanDev7 <adryanlimarj@gmail.com>
2026-06-03 17:22:09 -0300

    Esqueleto do Projeto

---
+++ src/main/java/br/com/adryan/SpringBoot_Estudo/SpringBootEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.adryan.SpringBoot_Estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/adryan/SpringBoot_Estudo/SpringBootEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adryan.SpringBoot_Estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
